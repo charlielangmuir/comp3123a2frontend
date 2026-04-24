dc30d73d2799206acafc7b02958d5737d746b574
charlielangmuir <charlie.langmuir@gmail.com>
2026-04-24 16:26:50 -0400

    added missing import

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axiosClient from "../api/axiosClient";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
